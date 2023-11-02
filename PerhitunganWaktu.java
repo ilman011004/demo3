a8b0e7986f00ea1c875be561c4e2745868a712f2
ilman nafian <ilmannafian587@gmail.com>
2023-11-02 11:02:15 +0700

    coba

---
+++ PerhitunganWaktu.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PerhitunganWaktu {
+    public static void main(String[] args) {
+        double jarak = 100;
+        double kecepatan = 50;
+
+
+        // Jarak dalam kilometer
+
+        // Kecepatan rata-rata dalam kilometer per jam
+
+
+/**
+ * rumus 
+ */
+        // Menghitung waktu dengan rumus jarak dibagi kecepatan
+        double waktu = jarak / kecepatan;
+
+        System.out.println("Jarak (km): " + jarak);
+        System.out.println("Kecepatan (km/jam): " + kecepatan);
+        System.out.println("Waktu (jam): " + waktu);
+
+    }
+}
+
+
+//berubah
